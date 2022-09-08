ebf3d9b89e96030220510e58cef96f191e60b4eb
Karol <karol@4-you.pl>
2022-09-08 04:01:53 +0200

    lab09

--- Labs/toLab10/lab09.py
+++ Labs/toLab10/lab09.py
@@ -1,13 +1,13 @@
 # Testing zone
 print('\nTesting zone\n')
-tax = (1, 2, 3, 'elo')
-taxList = list(tax)
-print(tax)
-taxList.append(6)
-# tax = taxList
-tax2 = ('test', 'alfa')
-tax = tax2
-print(tax)
+countryCodes ={'PL' : 'Poland', 'DE' : 'Germany', 'FR' : 'France', 'US' : 'USA'}
+print(countryCodes.get('RU'))
+countryCodes.setdefault('UK')
+print(countryCodes.get('PL'), countryCodes.get('UK'), sep='\t')
+print(countryCodes.get('RU'))
+print(countryCodes.setdefault('UA', 'Ukraine'))
+print(countryCodes.get('UA'))
+print(countryCodes.setdefault('UA', 'Ukraine test'))
 print('\nEnd Testing zone\n')
 # End Testing zone
 
@@ -26,3 +26,22 @@ print(emailMarketingList)
 
 tupleEmails = (emailMarketingList.copy())
 print(tupleEmails)
+
+# Note
+# a method to dictionary (dict)  print( dictName .setdefault(key, value) is checking that key has value
+# if not setting, a default value in other case showing currently value
+
+# Note 2
+# a method to dictionary (dict) dictName .update(dict) is extends of currently dictionary
+# about keys and value from dictionary indicated in brackets
+
+marketingChannels = {'Google' : 1480, 'Email' : 300, 'Natural traffic' : 440, 'TV Spot' : 700}
+print('Email result:', marketingChannels.get('Email'))
+currentResultsChannels = {'Natural traffic' : 520, 'News' : 600}
+marketingChannels.update(currentResultsChannels)
+print(marketingChannels.keys())
+print(marketingChannels.values())
+
+
+marketingChannels.pop('Email')
+print(marketingChannels.items())
