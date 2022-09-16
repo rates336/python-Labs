3da867f01b3493f0ea87e5c3f6e37f04e6b1d05b
Karol <karol@4-you.pl>
2022-09-16 02:12:42 +0200

    lab18

---
+++ Labs/toLab20/lab18.py
@@ -0,0 +1,31 @@
+import string
+
+poem = '''1.Runą i w łunach spłoną pożarnych 
+Krzyże kościołów, krzyże ofiarne 
+I w bezpowrotnym zgubi się szlaku 
+W lechickiej ziemi Orzeł Polaków. 
+2.O, jasne słońce- wodzu Stalinie! 
+Niech sława twoja nigdy nie zginie 
+Niechaj jak orły powiedzie z gniazda 
+Rosja i z Kremla płonąca gwiazda. 
+3.Na ziemskim globie flagi czerwone 
+Będą na wiatrach grały jak dzwony 
+Czerwona Armia i wódz jej Stalin 
+Odwiecznych wrogów na zawsze obali! 
+4.Zaćmisz się rychło w czarnej godzinie 
+Polsko- Twe córy i syny, 
+Wiara i każdy krzyż na mogile, 
+U stóp am legą w prochu i pyle! '''
+
+lines = poem.split('\n')
+linesAlt = []
+poemAlt = ''
+counter = 0
+print(lines)
+while counter < lines.__len__() / 2:
+    linesAlt.append(lines[counter])
+    linesAlt.append(lines[counter + 8])
+    counter += 1
+    poemAlt = '\n'.join(linesAlt)
+print(poemAlt)
+
