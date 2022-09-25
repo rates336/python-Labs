38658444a487e266b13348dc8bb5c7190afce07e
Karol <karol@4-you.pl>
2022-09-25 06:26:43 +0200

    lab32

--- Labs/toLab40/lab32.py
+++ Labs/toLab40/lab32.py
@@ -24,20 +24,27 @@ def get_all_chances_list(the_list=None, amount_to_return=0, return_more_or_low_q
         print('Wrong value sent to function')
         print('To function was send list:', the_list)
         return -1
-    len_the_list = len(the_list)
-    amount_to_return = len_the_list if amount_to_return == 0 else False
-    list_of_all_combinations = []
-    counter2 = 1
-    tmp_list = []
-    if True:
-        counter = to_strong(len_the_list) / (to_strong(amount_to_return) * to_strong(len_the_list - amount_to_return))
-        while counter > 0:
-            loop_list = []
-            for x in range(len_the_list):
-                tmp_list = [the_list[x]]
-                loop_list.append(tmp_list)
-            while counter2 < len_the_list:
+    return the_list[:amount_to_return]
+    # len_the_list = len(the_list)
+    # amount_to_return = len_the_list if amount_to_return == 0 else False
+    # list_of_all_combinations = []
+    # counter2 = 1
+    # tmp_list = []
+    # if True:
+    #     counter = to_strong(len_the_list) / (to_strong(amount_to_return) * to_strong(len_the_list - amount_to_return))
+    #     while counter > 0:
+    #         loop_list = []
+    #         for x in range(len_the_list):
+    #             tmp_list = [the_list[x]]
+    #             loop_list.append(tmp_list)
+    #         while counter2 < len_the_list:
 
 
-
+text = '''Korporacja (z łac. corpo – ciało, ratus – szczur; pol. ciało szczura) – organizacja, która pod przykrywką \
+prowadzenia biznesu włada dzisiejszym światem. Wydawać się może utopijnym miejscem realizacji pasji zawodowych. \
+W rzeczywistości jednak nie jest wcale tak kolorowo. Korporacja służy do wyzyskiwania człowieka w imię postępu. \
+Rządzi w niej prawo dżungli.
+'''
+print(get_all_chances_list(colors, 4))
+print(text[:text.find(')')].replace('(', ''))
 
