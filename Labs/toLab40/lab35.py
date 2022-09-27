9a77b9d256adcabf6245cf2527f4185805385b07
Karol <karol@4-you.pl>
2022-09-27 02:24:42 +0200

    lab35

--- Labs/toLab40/lab35.py
+++ Labs/toLab40/lab35.py
@@ -34,5 +34,35 @@ routes = [(start, stop) for start in ports for stop in ports if start < stop]
 print(routes)
 print(len(routes))
 
+# Task !30
+"""
+W tym zadaniu należy przerobić listy z poprzedniego LAB do postaci generatora. Nieco problematyczne 
+będzie ustalenie ile wartości jest generowanych przez generator, bo w tym celu... trzeba go przejść!
 
+Jeżeli taki opis Ci wystarcza to GO! GO! GO!
 
+A tu dokładniejsza instrukcja:
+Na przekręcie z wpłatomatem z poprzedniego zadania postanawiasz wraz ze swoim szefem otworzyć 
+linie lotnicze"Flying Python". Linie będą krajowe. Oto wykaz portów lotniczych:
+ports = ['WAW', 'KRK', 'GDN', 'KTW', 'WMI', 'WRO', 'POZ', 'RZE', 'SZZ',
+         'LUZ', 'BZG', 'LCJ', 'SZY', 'IEG', 'RDO']
+         
+1. Zbuduj generator tupletów symbolizujących port początkowy i końcowy. Wykonaj połączenie każdy-z-każdym
+2. Wyeliminuj z powyższego generatora połączenie z portu do tego samego portu
+3. Ponieważ połączenie z A do B dubluje się z połączeniem z B do A - 
+wygeneruj możliwe połączenia krajowe pomijając takie zdublowane trasy.
+4. Policz ilość generowanych połączeń w krokach 1,2,3. W tym celu napisz pętlę for, 
+która: wyświetli wygenerowane wartości i policzy je
+"""
+ports = ['WAW', 'KRK', 'GDN', 'KTW', 'WMI', 'WRO', 'POZ', 'RZE', 'SZZ',
+         'LUZ', 'BZG', 'LCJ', 'SZY', 'IEG', 'RDO']
+
+gen_routes = ((x, y) for x in ports for y in ports if x > y)
+print(next(gen_routes))
+print(next(gen_routes))
+counter = 2
+for route in gen_routes:
+    print(route)
+    counter += 1
+else:
+    print(counter)
