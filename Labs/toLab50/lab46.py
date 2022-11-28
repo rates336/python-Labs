25bece888c1186cd8ed911b079c1e916c4d0d636
Karol <karol@4-you.pl>
2022-11-29 00:44:39 +0100

    lab46 eddited

--- Labs/toLab50/lab46.py
+++ Labs/toLab50/lab46.py
@@ -14,29 +14,76 @@ print(list(map(lambda cake: '{} cake with {} glaze with text "{}" of {} kg'.form
     cake.get('taste'), cake.get('glaze'), cake.get('text'), cake.get('weight')), cakes_list)))
 
 
+def show_bakery_offer():
+    print(Cake.bakery_offer)
+
+def fill_bakery_offer():
+    Cake.bakery_offer.append(Cake('Happy cake', 'sponge', 'chocolate', ['icing-sugar', 'm&m', 'kitkat'],
+                                  'cherry', 'Happy Day :)', 'L'))
+    Cake.bakery_offer.append(Cake('Fit cake', 'cheese_cake', 'carrot', ['banana', 'kiwi', 'nuts'],
+                                  '', 'I am fit because I like it! :)', 'M'))
+    Cake.bakery_offer.append(Cake('Chocko-power', 'chocolate_cake', 'chocolate',
+                                  ['grated-chocolate', 'pieces of chocolate', 'white chocolate glaze'],
+                                  'sparkling chocolate', 'I love cho co\nla te ;Q', 'XXL'))
+    Cake.bakery_offer.append(Cake('Meringue', 'meringue', 'cream', [],
+                                  'vanilla', 'beZZa :p', 'M'))
+
+
 class Cake:
+    known_types = ['cake', 'muffin', 'pudding', 'meringue', 'cheese_cake', 'biscuit', 'waffle',
+                   'eclair', 'christmas_cake', 'pretzel', 'sponge', 'jelly', 'other']
+    bakery_offer = []
 
     def __init__(self, name: str, kind: str, carry_taste: str, additions: list, filling: str,
                  text: str, size: str):
         self.name = name
-        self.kind = kind
+        if Cake.known_types.count(kind) > 0:
+            self.kind = kind
+        else:
+            self.kind = Cake.known_types[-1]
         self.carry_taste = carry_taste
         self.additions = additions
         self.filling = filling
         self.text = text
         self.size = size
+        # fill_bakery_offer()
+        Cake.bakery_offer.append(Cake('Happy cake', 'sponge', 'chocolate', ['icing-sugar', 'm&m', 'kitkat'],
+                                  'cherry', 'Happy Day :)', 'L'))
+        Cake.bakery_offer.append(Cake('Fit cake', 'cheese_cake', 'carrot', ['banana', 'kiwi', 'nuts'],
+                                  '', 'I am fit because I like it! :)', 'M'))
+        Cake.bakery_offer.append(Cake('Chocko-power', 'chocolate_cake', 'chocolate',
+                                  ['grated-chocolate', 'pieces of chocolate', 'white chocolate glaze'],
+                                  'sparkling chocolate', 'I love cho co\nla te ;Q', 'XXL'))
+        Cake.bakery_offer.append(Cake('Meringue', 'meringue', 'cream', [],
+                                  'vanilla', 'beZZa :p', 'M'))
 
+    def info(self):
+        print(25 * '-')
+        print(self.name)
+        print('Kind: {}'.format(self.kind))
+        print('Carry taste: {}'.format(self.carry_taste))
+        print('Additions: {}\nFilling: {}'.format(self.additions, self.filling))
+        print('Some text: {}, Size: {}'.format(len(self.text) > 0, self.size))
+        print(25 * '-')
+        return self
 
-cake_03 = Cake('Happy cake', 'sponge_cake', 'chocolate', ['icing-sugar', 'm&m', 'kitkat'],
-               'cherry', 'Happy Day :)', 'L')
-cake_04 = Cake('Fit cake', 'cheese_cake', 'carrot', ['banana', 'kiwi', 'nuts'],
-               'nothing', 'I am fit because I like it! :)', 'M')
-cake_05 = Cake('Chocko-power', 'chocolate_cake', 'chocolate',
-               ['grated-chocolate', 'pieces of chocolate', 'white chocolate glaze'],
-               'sparkling chocolate', 'I love cho co\nla te ;Q', 'XXL')
+    def set_filling(self, filling):
+        self.filling = filling
 
-bakery_offer = [cake_03, cake_04, cake_05]
+    def add_additives(self, list_additions: list):
+        for x in list_additions:
+            self.additions.append(x)
+
 
 print('Today we offers:')
-print(list(map(lambda cake: cake.name + ' and it\'s a ' + cake.kind, bakery_offer)))
-# print(list(map(lambda cake: cake.name + '\nIt\'s a ' + cake.kind, bakery_offer)))
+print(list(map(lambda cake: cake.name + ' and it\'s a ' + cake.kind, Cake.bakery_offer)))
+# print(list(map(lambda cake: cake.name + '\nIt\'s a ' + (cake.kind + '\ '), bakery_offer)))
+cake_02 = Cake('Cocoa waffle', 'waffle', 'cocoa', [], 'cocoa', '', 'S')
+show_bakery_offer()
+Cake.bakery_offer[-1].info()
+Cake.bakery_offer[3].set_filling('2x vanilla')
+Cake.bakery_offer[-1].add_additives(['whipped cream'])
+Cake.bakery_offer[3].info()
+Cake.bakery_offer[2].info()
+Cake.bakery_offer.append(Cake('Cocoa waffle', 'waffle', 'cocoa', [], 'cocoa', '', 'S'))
+Cake.bakery_offer[-1].info()
