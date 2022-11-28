7f8bd52842848407eb35b276d8d62ca6d89ad4f5
Karol <karol@4-you.pl>
2022-11-28 18:18:18 +0100

    lab46

--- Labs/toLab50/lab46.py
+++ Labs/toLab50/lab46.py
@@ -11,6 +11,32 @@ def show_cake_info(cake: dict):
 show_cake_info(cake_00)
 show_cake_info(cakes_list[1])
 print(list(map(lambda cake: '{} cake with {} glaze with text "{}" of {} kg'.format(
-        cake.get('taste'), cake.get('glaze'), cake.get('text'), cake.get('weight')), cakes_list)))
+    cake.get('taste'), cake.get('glaze'), cake.get('text'), cake.get('weight')), cakes_list)))
 
 
+class Cake:
+
+    def __init__(self, name: str, kind: str, carry_taste: str, additions: list, filling: str,
+                 text: str, size: str):
+        self.name = name
+        self.kind = kind
+        self.carry_taste = carry_taste
+        self.additions = additions
+        self.filling = filling
+        self.text = text
+        self.size = size
+
+
+cake_03 = Cake('Happy cake', 'sponge_cake', 'chocolate', ['icing-sugar', 'm&m', 'kitkat'],
+               'cherry', 'Happy Day :)', 'L')
+cake_04 = Cake('Fit cake', 'cheese_cake', 'carrot', ['banana', 'kiwi', 'nuts'],
+               'nothing', 'I am fit because I like it! :)', 'M')
+cake_05 = Cake('Chocko-power', 'chocolate_cake', 'chocolate',
+               ['grated-chocolate', 'pieces of chocolate', 'white chocolate glaze'],
+               'sparkling chocolate', 'I love cho co\nla te ;Q', 'XXL')
+
+bakery_offer = [cake_03, cake_04, cake_05]
+
+print('Today we offers:')
+print(list(map(lambda cake: cake.name + ' and it\'s a ' + cake.kind, bakery_offer)))
+# print(list(map(lambda cake: cake.name + '\nIt\'s a ' + cake.kind, bakery_offer)))
