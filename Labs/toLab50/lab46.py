ae33425b60d3158a13a8ff5f9f0dfb132945a455
Karol <karol@4-you.pl>
2022-11-29 20:47:27 +0100

    lab 46

--- Labs/toLab50/lab46.py
+++ Labs/toLab50/lab46.py
@@ -14,8 +14,7 @@ print(list(map(lambda cake: '{} cake with {} glaze with text "{}" of {} kg'.form
     cake.get('taste'), cake.get('glaze'), cake.get('text'), cake.get('weight')), cakes_list)))
 
 
-def show_bakery_offer():
-    print(Cake.bakery_offer)
+
 
 def fill_bakery_offer():
     Cake.bakery_offer.append(Cake('Happy cake', 'sponge', 'chocolate', ['icing-sugar', 'm&m', 'kitkat'],
@@ -29,33 +28,31 @@ def fill_bakery_offer():
                                   'vanilla', 'beZZa :p', 'M'))
 
 
+def show_bakery_offer():
+    print(list(map(lambda cake: cake.info(), Cake.bakery_offer)))
+
+
 class Cake:
     known_types = ['cake', 'muffin', 'pudding', 'meringue', 'cheese_cake', 'biscuit', 'waffle',
                    'eclair', 'christmas_cake', 'pretzel', 'sponge', 'jelly', 'other']
     bakery_offer = []
 
+    __gluten_free = False
+
     def __init__(self, name: str, kind: str, carry_taste: str, additions: list, filling: str,
-                 text: str, size: str):
+                 text: str, size: str, __gluten_free=False):
         self.name = name
         if Cake.known_types.count(kind) > 0:
             self.kind = kind
         else:
-            self.kind = Cake.known_types[-1]
+            self.kind = 'other'
         self.carry_taste = carry_taste
         self.additions = additions
         self.filling = filling
         self.text = text
         self.size = size
-        # fill_bakery_offer()
-        Cake.bakery_offer.append(Cake('Happy cake', 'sponge', 'chocolate', ['icing-sugar', 'm&m', 'kitkat'],
-                                  'cherry', 'Happy Day :)', 'L'))
-        Cake.bakery_offer.append(Cake('Fit cake', 'cheese_cake', 'carrot', ['banana', 'kiwi', 'nuts'],
-                                  '', 'I am fit because I like it! :)', 'M'))
-        Cake.bakery_offer.append(Cake('Chocko-power', 'chocolate_cake', 'chocolate',
-                                  ['grated-chocolate', 'pieces of chocolate', 'white chocolate glaze'],
-                                  'sparkling chocolate', 'I love cho co\nla te ;Q', 'XXL'))
-        Cake.bakery_offer.append(Cake('Meringue', 'meringue', 'cream', [],
-                                  'vanilla', 'beZZa :p', 'M'))
+        Cake.bakery_offer.append(self)
+        self.__gluten_free = __gluten_free
 
     def info(self):
         print(25 * '-')
@@ -64,11 +61,14 @@ class Cake:
         print('Carry taste: {}'.format(self.carry_taste))
         print('Additions: {}\nFilling: {}'.format(self.additions, self.filling))
         print('Some text: {}, Size: {}'.format(len(self.text) > 0, self.size))
+        print('Gluten free: {}'.format(self.__gluten_free))
         print(25 * '-')
         return self
 
     def set_filling(self, filling):
         self.filling = filling
+        print('Completed set filling in cake: {}'.format(self))
+        self.info()
 
     def add_additives(self, list_additions: list):
         for x in list_additions:
@@ -78,8 +78,18 @@ class Cake:
 print('Today we offers:')
 print(list(map(lambda cake: cake.name + ' and it\'s a ' + cake.kind, Cake.bakery_offer)))
 # print(list(map(lambda cake: cake.name + '\nIt\'s a ' + (cake.kind + '\ '), bakery_offer)))
-cake_02 = Cake('Cocoa waffle', 'waffle', 'cocoa', [], 'cocoa', '', 'S')
+cake_03 = Cake('Happy cake', 'sponge_cake', 'chocolate', ['icing-sugar', 'm&m', 'kitkat'],
+               'cherry', 'Happy Day :)', 'L')
+cake_03.info()
+# Cake.bakery_offer.append(cake_03)
+Cake('Cocoa waffle', 'waffle', 'cocoa', [], 'cocoa', '', 'S', True)
+# cake_02 = Cake('Cocoa waffle', 'waffle', 'cocoa', [], 'cocoa', '', 'S')
+# Cake.__init__(cake_03, 'Cocoa waffle', 'waffle', 'cocoa', [], 'cocoa', '', 'L')
 show_bakery_offer()
+fill_bakery_offer()
+print(40 * '*')
+show_bakery_offer()
+print(Cake.bakery_offer)
 Cake.bakery_offer[-1].info()
 Cake.bakery_offer[3].set_filling('2x vanilla')
 Cake.bakery_offer[-1].add_additives(['whipped cream'])
@@ -87,3 +97,15 @@ Cake.bakery_offer[3].info()
 Cake.bakery_offer[2].info()
 Cake.bakery_offer.append(Cake('Cocoa waffle', 'waffle', 'cocoa', [], 'cocoa', '', 'S'))
 Cake.bakery_offer[-1].info()
+
+print(isinstance(cake_01, Cake))
+print(isinstance(cake_03, Cake))
+print(type(cake_03) is Cake)
+print('vars Cake: {} \ndir Cake: {}'.format(vars(Cake), dir(Cake)))
+print('vars cake_03: {} \ndir cake_03: {}'.format(vars(cake_03), dir(cake_03)))
+print(vars(cake_03))
+cake_03._Cake__gluten_free = True
+print(vars(cake_03))
+cake_03._Cake__gluten_free = False
+print(vars(cake_03))
+cake_03.__gluten_free = True
